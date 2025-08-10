04ab8bae5d2ae25d7e3b3d31afbc5b12b944da64
bahsoonagroprocessingindustries. <37225677+bahsoonagro@users.noreply.github.com>
2025-08-10 10:47:57 +0000

    lklk

--- server.js
+++ server.js
@@ -6,6 +6,9 @@ import stockItemRoutes from './routes/stockItemRoutes.js';
 import rawMaterialRoutes from './routes/rawMaterialRoutes.js';
 
 const app = express();
+const PORT = process.env.PORT || 5000;
+const MONGO_URI = process.env.MONGO_URI;
+
 
 app.use(cors());
 app.use(express.json());
@@ -28,8 +31,7 @@ app.listen(PORT, () => console.log(`🚀 Server running on port ${PORT}`));
 
 dotenv.config();
 
-const PORT = process.env.PORT || 5000;
-const MONGO_URI = process.env.MONGO_URI;
+
 
 mongoose
   .connect(MONGO_URI)
