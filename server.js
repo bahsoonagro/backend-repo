e8b75d2bf11368bd1c9608bf4c25f38b8600c4f5
bahsoonagroprocessingindustries. <37225677+bahsoonagro@users.noreply.github.com>
2025-08-10 10:37:48 +0000

    jk

--- server.js
+++ server.js
@@ -26,18 +26,6 @@ mongoose.connect(process.env.MONGO_URI)
 
 app.listen(PORT, () => console.log(`🚀 Server running on port ${PORT}`));
 
-
-
-
-
-
-
-
-
-
-
-
-
 dotenv.config();
 
 const PORT = process.env.PORT || 5000;
