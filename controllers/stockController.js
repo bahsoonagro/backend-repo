0799d8c45557780447c171d5b7889f7c733e8c3c
bahsoonagroprocessingindustries. <37225677+bahsoonagro@users.noreply.github.com>
2025-08-10 09:39:31 +0000

    hkvkh

--- controllers/stockController.js
+++ controllers/stockController.js
@@ -1,20 +1,20 @@
-import Stock from '../models/Stock.js';
+// routes/stockRoutes.js
+import express from 'express';
+import {
+  getAllStocks,
+  createStock,
+  deleteStock
+} from '../controllers/stockController.js';
 
-export const getAllStocks = async (req, res) => {
-  try {
-    const stocks = await Stock.find();
-    res.json(stocks);
-  } catch (err) {
-    res.status(500).json({ message: err.message });
-  }
-};
+const router = express.Router();
 
-export const createStock = async (req, res) => {
-  const stock = new Stock(req.body);
-  try {
-    const newStock = await stock.save();
-    res.status(201).json(newStock);
-  } catch (err) {
-    res.status(400).json({ message: err.message });
-  }
-};
+// GET all stocks
+router.get('/', getAllStocks);
+
+// POST create new stock
+router.post('/', createStock);
+
+// DELETE stock by ID
+router.delete('/:id', deleteStock);
+
+export default router;
