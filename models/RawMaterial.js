d4e0ae7a490c072917bd1f04d73819d3c304d3e2
bahsoonagroprocessingindustries. <37225677+bahsoonagro@users.noreply.github.com>
2025-09-01 00:02:01 +0000

    Update RawMaterial.js

--- models/RawMaterial.js
+++ models/RawMaterial.js
@@ -2,11 +2,17 @@ import mongoose from "mongoose";
 
 const rawMaterialSchema = new mongoose.Schema({
   rawMaterialType: { type: String, required: true },
-  date: { type: Date, required: true },
+  supplierName: { type: String, required: true },
+  supplierPhone: { type: String, required: true },
+  supplierBags: { type: Number, required: true },
+  extraKg: { type: Number, default: 0 },
+  bagsAfterStd: { type: Number, required: true },
+  totalWeight: { type: Number, required: true },
   storeKeeper: { type: String, required: true },
   supervisor: { type: String, required: true },
   location: { type: String, required: true },
-  weightKg: { type: Number, required: true },
+  batchNumber: { type: String, required: true },
+  date: { type: Date, required: true },
   damaged: { type: String, enum: ["Yes", "No"], default: "No" },
   createdAt: { type: Date, default: Date.now }
 });
