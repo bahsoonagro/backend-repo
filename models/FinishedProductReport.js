f5e5bb232935cc62a24fb71e82631e4d983d1861
bahsoonagroprocessingindustries. <37225677+bahsoonagro@users.noreply.github.com>
2025-09-01 10:53:19 +0000

    Create FinishedProductReport.js

---
+++ models/FinishedProductReport.js
@@ -0,0 +1,15 @@
+{
+  month: String,
+  week: String,
+  date: Date,
+  rows: [
+    {
+      productName: String,
+      stockRef: String,
+      openingStock: Number,
+      stockIn: Number,
+      stockOut: Number,
+      totalQuantity: Number
+    }
+  ]
+}
