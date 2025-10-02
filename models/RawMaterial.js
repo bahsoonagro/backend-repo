996b2ca5de1f023d5dad426884a5d9768161772b
bahsoonagroprocessingindustries. <37225677+bahsoonagro@users.noreply.github.com>
2025-10-02 15:16:18 +0000

    Update RawMaterial.js

--- models/RawMaterial.js
+++ models/RawMaterial.js
@@ -1,21 +1,19 @@
 import mongoose from "mongoose";
 
 const rawMaterialSchema = new mongoose.Schema({
-  rawMaterialType: { type: String, required: true },
-  supplierName: { type: String, required: true },
-  supplierPhone: { type: String, required: true },
-  supplierBags: { type: Number, required: true },
-  extraKg: { type: Number, default: 0 },
-  bagsAfterStd: { type: Number, required: true },
-  totalWeight: { type: Number, required: true },
+  date: { type: Date, required: true },
+  openingBal: { type: Number, required: true, default: 0 },
+  newStock: { type: Number, required: true, default: 0 },
+  totalStock: { type: Number, required: true, default: 0 },
+  stockOut: { type: Number, required: true, default: 0 },
+  balance: { type: Number, required: true, default: 0 },
+  remarks: { type: String, default: "" },
+  requisitionNumber: { type: String, default: "" },
   storeKeeper: { type: String, required: true },
   supervisor: { type: String, required: true },
-  location: { type: String, required: true },
   batchNumber: { type: String, required: true },
-  date: { type: Date, required: true },
-  damaged: { type: String, enum: ["Yes", "No"], default: "No" },
-  createdAt: { type: Date, default: Date.now }
-});
+  location: { type: String, required: true },
+}, { timestamps: true });
 
 const RawMaterial = mongoose.model("RawMaterial", rawMaterialSchema);
 export default RawMaterial;
