0ebc61b28f10682d7b0effba70399754afc3de6b
bahsoonagroprocessingindustries. <37225677+bahsoonagro@users.noreply.github.com>
2025-10-02 15:18:34 +0000

    Update rawMaterialRoutes.js

--- routes/rawMaterialRoutes.js
+++ routes/rawMaterialRoutes.js
@@ -1,36 +1,17 @@
-// routes/rawMaterialRoutes.js
 import express from "express";
-import { 
-  addRawMaterial, 
-  bulkUploadRawMaterials, 
-  getLPOs, 
-  addLPO, 
-  deleteLPO,
-  getAllRawMaterials
+import {
+  addRawMaterial,
+  getAllRawMaterials,
+  deleteRawMaterial,
+  bulkUploadRawMaterials
 } from "../controllers/rawMaterialController.js";
 
 const router = express.Router();
 
-// ----------------- RAW MATERIALS -----------------
-
-// Add a single raw material
+// Raw Materials Routes
 router.post("/", addRawMaterial);
-
-// Get all raw materials
 router.get("/", getAllRawMaterials);
-
-// Bulk upload raw materials
+router.delete("/:id", deleteRawMaterial);
 router.post("/bulk-upload", bulkUploadRawMaterials);
-
-// ----------------- LPO ROUTES -----------------
-
-// Get all LPOs
-router.get("/lpo", getLPOs);
-
-// Add a new LPO
-router.post("/lpo", addLPO);
-
-// Delete an LPO by ID
-router.delete("/lpo/:id", deleteLPO);
 
 export default router;
