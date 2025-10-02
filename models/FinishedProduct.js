701b882729a2358873a8f3a770508a62065d6f22
bahsoonagroprocessingindustries. <37225677+bahsoonagro@users.noreply.github.com>
2025-10-02 12:12:31 +0000

    Update FinishedProduct.js

--- models/FinishedProduct.js
+++ models/FinishedProduct.js
@@ -2,11 +2,14 @@
 import mongoose from "mongoose";
 
 const finishedProductSchema = new mongoose.Schema({
-  productName: { type: String, required: true },
   batchNumber: { type: String, required: true },
-  productionDate: { type: Date, required: true },
-  quantityKg: { type: Number, required: true },
-  unit: { type: String, required: true },
+  date: { type: Date, required: true },
+  productName: { type: String, required: true },
+  openingQty: { type: Number, required: true },
+  newStock: { type: Number, required: true },
+  totalStock: { type: Number, required: true },
+  qtyOut: { type: Number, required: true },
+  balance: { type: Number, required: true },
   remarks: { type: String }
 }, { timestamps: true });
 
