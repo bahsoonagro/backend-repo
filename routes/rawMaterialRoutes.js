7ed2de331a35d7e713d53afed365fc25fc8efa0c
bahsoonagroprocessingindustries. <37225677+bahsoonagro@users.noreply.github.com>
2025-10-04 07:03:05 +0000

    Update rawMaterialRoutes.js

--- routes/rawMaterialRoutes.js
+++ routes/rawMaterialRoutes.js
@@ -2,15 +2,16 @@ import express from "express";
 import {
   addRawMaterial,
   getAllRawMaterials,
+  updateRawMaterial,
   deleteRawMaterial,
   bulkUploadRawMaterials
 } from "../controllers/rawMaterialController.js";
 
 const router = express.Router();
 
-// Raw Materials Routes
 router.post("/", addRawMaterial);
 router.get("/", getAllRawMaterials);
+router.put("/:id", updateRawMaterial); // NEW
 router.delete("/:id", deleteRawMaterial);
 router.post("/bulk-upload", bulkUploadRawMaterials);
 
